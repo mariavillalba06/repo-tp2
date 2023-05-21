d8c370f31fbe237ba3611b66a7f19c6af1aa1a46
SebasMullicundo <122728796+SebasMullicundo@users.noreply.github.com>
2023-05-21 09:01:34 -0300

    creacion controller, model y uso thymeleaf en la pagina servicios.html

---
+++ tp2_Grupo37/src/main/java/ar/edu/unju/fi/listas/ListaServicioPaseo.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.listas;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.model.ServicioPaseo;
+
+public class ListaServicioPaseo {
+	private List<ServicioPaseo> servicioPaseos;
+	
+	public ListaServicioPaseo() {
+		servicioPaseos = new ArrayList<ServicioPaseo>();
+		
+		servicioPaseos.add(new ServicioPaseo(1, "Juan Perez", "Lunes", (byte)9, (byte)12));
+		servicioPaseos.add(new ServicioPaseo(2, "Lucas Diaz", "Martes", (byte)16, (byte)18));
+		servicioPaseos.add(new ServicioPaseo(3, "Juan Perez", "Miercoles", (byte)9, (byte)12));
+		servicioPaseos.add(new ServicioPaseo(4, "María Gomez", "Jueves", (byte)16, (byte)18));
+		servicioPaseos.add(new ServicioPaseo(5, "Lucas Diaz", "Viernes", (byte)9, (byte)12));
+		servicioPaseos.add(new ServicioPaseo(6, "Roberto Vazquez", "Sábado", (byte)16, (byte)18));
+	}
+	
+	public List<ServicioPaseo> getServicioPaseos() {
+		return servicioPaseos;
+	}
+	
+	public void setServicioPaseos(List<ServicioPaseo> servicioPaseos) {
+		this.servicioPaseos = servicioPaseos;
+	}
+}
