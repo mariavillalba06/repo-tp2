19fb564033f7febe649b40d68e18d7c5ad46ca4c
SebasMullicundo <122728796+SebasMullicundo@users.noreply.github.com>
2023-05-25 16:58:36 -0300

    Adicion de comentarios al modelo, controlador y template de servicios

--- tp2_Grupo37/src/main/java/ar/edu/unju/fi/listas/ListaServicioCorte.java
+++ tp2_Grupo37/src/main/java/ar/edu/unju/fi/listas/ListaServicioCorte.java
@@ -10,16 +10,23 @@ import java.util.ArrayList;
 public class ListaServicioCorte {
 	private List<ServicioCorte> servicioCortes;
 	
+	/**
+     * Constructor de la clase ListaServicioCorte
+     * Permite inicializar la lista de servicioCortes con algunos bojetos percargados
+     */
 	public ListaServicioCorte() {
 		servicioCortes = new ArrayList<ServicioCorte>();
 		
-		servicioCortes.add(new ServicioCorte(1,(byte)0, (byte)2,"Tijera",1000));
-		servicioCortes.add(new ServicioCorte(2,(byte)3, (byte)9,"Máquina",2000));
+		servicioCortes.add(new ServicioCorte(1,(byte)1, (byte)4,"Tijera",1000));
+		servicioCortes.add(new ServicioCorte(2,(byte)5, (byte)9,"Máquina",2000));
 		servicioCortes.add(new ServicioCorte(3,(byte)10, (byte)14,"Tijera",3000));
 		servicioCortes.add(new ServicioCorte(4,(byte)15, (byte)19,"Tijera",4000));
 		servicioCortes.add(new ServicioCorte(5,(byte)20, (byte)24,"Máquina",5000));
 	}
 	
+	/**
+     * Retorna la lista de servicioCortes
+     */
 	public List<ServicioCorte> getServicioCortes() {
 		return servicioCortes;
 	}
